5cb0fe773c2d08955c1f71c13021d1046cfadacd
Nilesh Makwana <40433354+nilesh52@users.noreply.github.com>
2020-07-08 09:31:07 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "AIzaSyBGUoI6E35RFwcQ_dfU7LHN0cv_qXpB2ZI",
-    authDomain: "oshop-1bd86.firebaseapp.com",
-    databaseURL: "https://oshop-1bd86.firebaseio.com",
-    projectId: "oshop-1bd86",
-    storageBucket: "oshop-1bd86.appspot.com",
-    messagingSenderId: "377518313607",
-    appId: "1:377518313607:web:56fcb8edfb9dc5f4ffe822",
-    measurementId: "G-67335F2LK1"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
